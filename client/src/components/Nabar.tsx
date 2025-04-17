01a2b0a375ff47a1d91bd82159a31e4d67639b7e
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2025-04-18 00:34:00 +0530

    mobile nav

--- client/src/components/Nabar.tsx
+++ client/src/components/Nabar.tsx
@@ -10,7 +10,16 @@ import {
   FileChartLine,
   FileQuestion,
   Briefcase,
+  Menu,
 } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 
 export default function Navbar() {
@@ -85,29 +94,81 @@ export default function Navbar() {
         </nav>
 
         <div className="md:hidden">
-          <div className="flex space-x-4">
-            <Link
-              href="/"
-              className={cn(
-                "p-2 rounded-md",
-                isActive("/") ? "bg-brand-50 text-brand-600" : "text-gray-600"
-              )}
-            >
-              <FileText className="h-5 w-5" />
-            </Link>
+          <Sheet>
+            <SheetTrigger className="p-2 rounded-md text-gray-600 hover:bg-gray-100">
+              <Menu className="h-5 w-5" />
+            </SheetTrigger>
+            <SheetContent side="right" className="w-64 pt-10">
+              <SheetHeader>
+                <SheetTitle></SheetTitle>
+              </SheetHeader>
+              <nav className="flex flex-col justify-center pl-6 space-y-7">
+                <Link
+                  href="/"
+                  className={cn(
+                    "flex items-center space-x-1 text-md font-medium transition-colors hover:text-brand-600",
+                    isActive("/")
+                      ? "text-brand-600 border-brand-300 p-3 max-w-44 border-[1px] rounded-xl"
+                      : "text-gray-600"
+                  )}
+                >
+                  <FileText className="h-4 w-4" />
+                  <span>Resume Review</span>
+                </Link>
 
-            <Link
-              href="/emails"
-              className={cn(
-                "p-2 rounded-md",
-                isActive("/emails")
-                  ? "bg-brand-50 text-brand-600"
-                  : "text-gray-600"
-              )}
-            >
-              <Send className="h-5 w-5" />
-            </Link>
-          </div>
+                <Link
+                  href="/email"
+                  className={cn(
+                    "flex items-center space-x-1 text-md font-medium transition-colors hover:text-brand-600",
+                    isActive("/email")
+                      ? "text-brand-600 border-brand-300 p-3 max-w-44 border-[1px] rounded-xl"
+                      : "text-gray-600"
+                  )}
+                >
+                  <Send className="h-4 w-4" />
+                  <span>Email</span>
+                </Link>
+
+                <Link
+                  href="/coverletter"
+                  className={cn(
+                    "flex items-center space-x-1 text-md font-medium transition-colors hover:text-brand-600",
+                    isActive("/coverletter")
+                      ? "text-brand-600 border-brand-300 p-3 max-w-44 border-[1px] rounded-xl"
+                      : "text-gray-600"
+                  )}
+                >
+                  <LetterText className="h-4 w-4" />
+                  <span>Cover Letter</span>
+                </Link>
+
+                <Link
+                  href="/jobanalyze"
+                  className={cn(
+                    "flex items-center space-x-1 text-md font-medium transition-colors hover:text-brand-600",
+                    isActive("/jobanalyze")
+                      ? "text-brand-600 border-brand-300 p-3 max-w-44 border-[1px] rounded-xl"
+                      : "text-gray-600"
+                  )}
+                >
+                  <Briefcase className="h-4 w-4" />
+                  <span>Job Analyze</span>
+                </Link>
+                <Link
+                  href="/prep"
+                  className={cn(
+                    "flex items-center space-x-1 text-md font-medium transition-colors hover:text-brand-600",
+                    isActive("/prep")
+                      ? "text-brand-600 border-brand-300 p-3 max-w-44 border-[1px] rounded-xl"
+                      : "text-gray-600"
+                  )}
+                >
+                  <FileQuestion className="h-4 w-4" />
+                  <span>Preparation QA</span>
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </header>
